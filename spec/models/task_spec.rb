42e177cb24441b7d926e968776335fda5dfbef92
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 13:23:20 -0300

    validates presence of name and description test in task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,1 +1,15 @@
 require "rails_helper"
+
+RSpec.describe Task, type: :model do
+    it "has a title" do
+        task = Task.new(title: "Testing task title", description: "Testing task description")
+        expect(task.title).to eq("Testing task title")
+      end
+  end
+
+RSpec.describe Task, type: :model do
+    it "has a description" do
+        task = Task.new(title: "Testing task title", description: "Testing task description")
+        expect(task.description).to eq("Testing task description")
+    end
+end
