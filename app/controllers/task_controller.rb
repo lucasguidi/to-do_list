cae20aaf1107194d20cf0d55f673c054188b7d8b
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 12:55:21 -0300

    writing actions in task_controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,11 +1,19 @@
 class TaskController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
+    @task = Task.new
   end
 
   def create
-  end
-
-  def index
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -15,8 +23,21 @@ class TaskController < ApplicationController
   end
 
   def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   def show
+  end
+
+  private
+
+  def get_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :private, :favorit, :done)
   end
 end
