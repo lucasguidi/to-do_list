0c04c18bea0b06da3106073e7bbd2fb9d6626fda
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 12:33:23 -0300

    controller list created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'list/index'
+  get 'list/new'
+  get 'list/create'
+  get 'list/destroy'
+  get 'list/edit'
+  get 'list/update'
+  get 'list/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
