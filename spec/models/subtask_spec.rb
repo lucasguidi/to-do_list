9e2859b99e147cf1168c54a3ae640b16f03754d3
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 13:23:49 -0300

    validates presence of name and description test in subtask model

--- spec/models/subtask_spec.rb
+++ spec/models/subtask_spec.rb
@@ -1,1 +1,15 @@
 require "rails_helper"
+
+RSpec.describe Subtask, type: :model do
+    it "has a title" do
+        subtask = Subtask.new(title: "Testing subtask title", description: "Testing subtask description")
+        expect(subtask.title).to eq("Testing subtask title")
+      end
+  end
+
+RSpec.describe Subtask, type: :model do
+    it "has a description" do
+        subtask = Subtask.new(title: "Testing subtask title", description: "Testing subtask description")
+        expect(subtask.description).to eq("Testing subtask description")
+    end
+end
