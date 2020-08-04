73cc5ed41e9a8fe89ee56e5bfbafe79687627573
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 12:55:56 -0300

    writing actions in subtask_controller

--- app/controllers/subtask_controller.rb
+++ app/controllers/subtask_controller.rb
@@ -1,11 +1,19 @@
 class SubtaskController < ApplicationController
+  def index
+    @subtasks = Subtask.all
+  end
+
   def new
+    @subtask = Subtask.new
   end
 
   def create
-  end
-
-  def index
+    @subtask = Subtask.new(subtask_params)
+    if @subtask.save
+      redirect_to subtask_path(@subtask)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -15,8 +23,21 @@ class SubtaskController < ApplicationController
   end
 
   def update
+    @subtask.update(subtask_params)
+
+    redirect_to subsubtask_path(@subtask)
   end
 
   def show
+  end
+
+  private
+
+  def get_subtask
+    @subtask = subtask.find(params[:id])
+  end
+
+  def subtask_params
+    params.require(:subtask).permit(:title, :description, :private, :favorit, :done)
   end
 end
