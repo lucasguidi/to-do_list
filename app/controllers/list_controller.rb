3f5b558d67d1144a3fc9f795a8c5b3273d95b9e8
Lucas Guidi <lukas.guidi@gmail.com>
2020-08-04 12:49:52 -0300

    writing actions in list_controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,11 +1,21 @@
 class ListController < ApplicationController
+  before_action :get_list, only: %i(show update destroy edit)
+
   def index
+    @lists = List.all
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -15,8 +25,21 @@ class ListController < ApplicationController
   end
 
   def update
+    @list.update(list_params)
+
+    redirect_to list_path(@list)
   end
 
   def show
+  end
+
+  private
+
+  def get_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :description, :private, :favorit, :done)
   end
 end
